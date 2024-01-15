1a515ec562faf653b4eeac6328d99b121b1c53d1
khalatesonali <khalatesonali1234@gmail.com>
2024-01-15 14:43:26 +0530

    BDDFramework for OrnageHRM created by Team Leader

---
+++ FirstBDDFrameworkTLrole/src/test/java/TestRunner/RunnerTest.java
@@ -0,0 +1,25 @@
+package TestRunner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		
+		features= {"AllFeaturesFile"},
+		glue= {"StepDefination"},
+		dryRun=false,
+		monochrome=true
+		
+		
+		
+		
+		
+		)
+
+
+
+
+
+public class RunnerTest extends AbstractTestNGCucumberTests {
+
+}
